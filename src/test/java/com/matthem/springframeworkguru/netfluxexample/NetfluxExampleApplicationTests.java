482b9d8d50e23ecd411d545f91e41ae1861ce952
Matthew Hemingway <dpplrdr435@gmail.com>
2020-08-12 18:00:35 -0400

    Initial Commit

---
+++ src/test/java/com/matthem/springframeworkguru/netfluxexample/NetfluxExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matthem.springframeworkguru.netfluxexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetfluxExampleApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
