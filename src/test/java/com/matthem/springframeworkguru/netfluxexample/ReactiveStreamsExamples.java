0b3e0eacbc4457bbced287cdacba3f355cea4955
Matthew Hemingway <dpplrdr435@gmail.com>
2020-08-13 18:11:34 -0400

    add reactive streams examples

---
+++ src/test/java/com/matthem/springframeworkguru/netfluxexample/ReactiveStreamsExamples.java
@@ -0,0 +1,142 @@
+package com.matthem.springframeworkguru.netfluxexample;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+public class ReactiveStreamsExamples {
+  @Test
+  public void simpleStreamExample() {
+    Flux<String> dogs = Flux.just("Vizsla", "Lab", "Golden", "GSP", "Poodle", "Yorkie", "Mutt");
+
+    dogs.toStream()
+        .forEach(System.out::println);
+  }
+
+  @Test
+  public void simpleStreamExample2() {
+    Flux<String> dogs = Flux.just("Vizsla", "Lab", "Golden", "GSP", "Poodle", "Yorkie", "Mutt");
+
+    dogs.subscribe(System.out::println);
+  }
+
+  @Test
+  public void simpleStreamExample3() {
+    Flux<String> dogs = Flux.just("Vizsla", "Lab", "Golden", "GSP", "Poodle", "Yorkie", "Mutt");
+
+    //no output; not subscribe action
+    dogs.doOnEach(dog -> System.out.println(dog.get()));
+  }
+
+  @Test
+  public void simpleStreamExample4() {
+    Flux<String> dogs = Flux.just("Vizsla", "Lab", "Golden", "GSP", "Poodle", "Yorkie", "Mutt");
+
+    //trigger subscription
+    dogs.doOnEach(dog -> System.out.println(dog.get())).subscribe();
+  }
+
+  @Test
+  public void simpleStreamExample5WithSubscriber() {
+    Flux<String> dogs = Flux.just("Vizsla", "Lab", "Golden", "GSP", "Poodle", "Yorkie", "Mutt");
+
+    //trigger subscription
+    dogs.subscribe((System.out::println), null, (() -> {System.out.println("Woot! all Done");}));
+  }
+
+  @Test
+  public void simpleStreamExample5WithSubscriberConsumers() {
+    Flux<String> dogs = Flux.just("Vizsla", "Lab", "Golden", "GSP", "Poodle", "Yorkie", "Mutt");
+
+    //subscriber lambda
+    Consumer<String> println = System.out::println;
+
+    //error handler
+    Consumer<Throwable> errorHandler = e -> System.out.println("Some Error Occurred");
+
+    //runnable upon complete
+    Runnable allDone = () -> System.out.println("Woot! All Done!");
+
+    //trigger subscription
+    dogs.subscribe(println, errorHandler, allDone);
+  }
+
+  @Test
+  public void mapStreamExample() {
+    Flux<String> dogs = Flux.just("Vizsla", "Lab", "Golden", "GSP", "Poodle", "Yorkie", "Mutt");
+
+    dogs.map(String::length)
+        .doOnEach(System.out::println)
+        .subscribe();
+  }
+
+  @Test
+  public void filterStreamExample() {
+    Flux<String> dogs = Flux.just("Vizsla", "Lab", "Golden", "GSP", "Poodle", "Yorkie", "Mutt");
+
+    dogs.filter(s -> s.length() == 6)
+        .subscribe(System.out::println);
+  }
+
+  @Test
+  public void filterAndLimitStreamExample() {
+    Flux<String> dogs = Flux.just("Vizsla", "Lab", "Golden", "GSP", "Poodle", "Yorkie", "Mutt");
+
+    dogs.filter(s -> s.length() == 6)
+        .take(2) //limit elements
+        .subscribe(System.out::println);
+  }
+
+  @Test
+  public void filterAndSortStreamExample() {
+    Flux<String> dogs = Flux.just("Vizsla", "Lab", "Golden", "GSP", "Poodle", "Yorkie", "Mutt");
+
+    dogs.filter(s -> s.length() == 6)
+        .take(2) //limit elements
+        .sort()
+        .subscribe(System.out::println);
+  }
+
+  @Test
+  public void filterAndSortStreamWithCollectorExample() {
+    Flux<String> dogs = Flux.just("Vizsla", "Lab", "Golden", "GSP", "Poodle", "Yorkie", "Mutt");
+
+    dogs.filter(s -> s.length() == 6)
+        .take(3) //limit elements
+        .sort()
+        .collect(Collectors.joining(", ")) //converts to Mono<String>
+        .subscribe(System.out::println);
+  }
+
+  @Test
+  public void testFlatMap() {
+    Flux<List<List<Integer>>> listFlux = Flux.just(Arrays.asList(Arrays.asList(1, 2, 3), Arrays.asList(4, 5, 6)));
+
+    listFlux.flatMap(Flux::fromIterable)
+        .flatMap(Flux::fromIterable)
+        .subscribe(System.out::println);
+  }
+
+  @Test
+  public void testFlatMap2() {
+    Flux<List<List<Integer>>> listFlux = Flux.just(Arrays.asList(Arrays.asList(1, 2, 3), Arrays.asList(4, 5, 6)));
+
+    listFlux.flatMap(lists -> Flux.fromIterable(
+        (lists.stream()
+            .flatMap(Collection::stream)
+        ).collect(Collectors.toList())))
+        .subscribe(System.out::println);
+  }
+
+  @Test
+  public void testReduction() {
+    Flux<String> dogs = Flux.just("Vizsla", "Lab", "Golden", "GSP", "Poodle", "Yorkie", "Mutt");
+
+    dogs.reduce((a, b) -> a + " - " + b).subscribe(System.out::println);
+  }
+}
