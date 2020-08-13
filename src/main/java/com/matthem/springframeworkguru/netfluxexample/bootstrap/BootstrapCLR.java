e542fb9d70ed57fb11f67234e8ab7fc6b15c7ce6
Matthew Hemingway <dpplrdr435@gmail.com>
2020-08-12 21:34:22 -0400

    Data Setup

---
+++ src/main/java/com/matthem/springframeworkguru/netfluxexample/bootstrap/BootstrapCLR.java
@@ -0,0 +1,32 @@
+package com.matthem.springframeworkguru.netfluxexample.bootstrap;
+
+import com.matthem.springframeworkguru.netfluxexample.domain.Movie;
+import com.matthem.springframeworkguru.netfluxexample.repositories.MovieRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+
+import java.util.UUID;
+
+@Component
+public class BootstrapCLR implements CommandLineRunner {
+  private final MovieRepository movieRepository;
+
+  public BootstrapCLR(MovieRepository movieRepository) {
+    this.movieRepository = movieRepository;
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    movieRepository.deleteAll()
+        .thenMany(
+            Flux.just("Silence of the Lambdas", "AEon Flux", "Enter the Mono<Void>", "The Fluxxinator",
+                "Back to the Future", "Meet the Fluxes", "Lord of the Fluxes")
+                .map(title -> new Movie(title, UUID.randomUUID().toString()))
+                .flatMap(movieRepository::save))
+        .subscribe(null, null, () ->
+            movieRepository.findAll().subscribe(System.out::println)
+        );
+
+  }
+}
--- src/main/java/com/matthem/springframeworkguru/netfluxexample/repositories/MovieRepository.java
+++ src/main/java/com/matthem/springframeworkguru/netfluxexample/repositories/MovieRepository.java
@@ -2,6 +2,8 @@ package com.matthem.springframeworkguru.netfluxexample.repositories;
 
 import com.matthem.springframeworkguru.netfluxexample.domain.Movie;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MovieRepository extends ReactiveMongoRepository<Movie, String> {
 }
